f3e65ec7a9c0757237e802d7b785651f6e065743
OweT1 <owentan@hotmail.sg>
2026-01-21 00:15:13 +0800

    feat: edited prompt for 'NOT_RELEVANT' for language scenarios

--- src/helper/llm/prompts.py
+++ src/helper/llm/prompts.py
@@ -32,7 +32,11 @@ def get_category_prompt(job_details: str) -> str:
     - The role is of a senior/lead/principal/expert/managerial/president or similar position, or requires minimally 2 years of working experience.
 
     You MUST categorise the job as 'NOT_RELEVANT' if any of the conditions are met:
-    - The role matches any of the technology roles, BUT it requires candidates to have working proficiency in languages that are not English or Chinese/Mandarin. For example, if a job requires working proficiency in Malay/Korean/Japanese or any other language that is not English or Chinese/Mandarin, you should categorise the job as 'NOT_RELEVANT'.
+    - The role matches any of the technology roles, BUT it requires candidates to have mandatory working proficiency in languages other than English or Chinese/Mandarin.
+        - If the job requires working proficiency in Malay, Thai, Korean, Japanese or any language other than English or Chinese/Mandarin, it should be categorised as 'NOT_RELEVANT'.
+        - If the job requires only English and/or Chinese/Mandarin, it should not be categorised as 'NOT_RELEVANT'.
+        - If the job lists languages outside of English and Chinese/Mandarin as good-to-have, it should not be categorised as 'NOT_RELEVANT'.
+        - If the job requires a secondary language and all required languages are within English and/or Chinese/Mandarin, then it should not be categorised as 'NOT_RELEVANT'.
     - The role does not clearly match any technology role in the Job Categories.
     </special_instructions>
 
