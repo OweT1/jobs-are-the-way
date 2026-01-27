368e72bec6cb3c90de044611e9f2995d05a2c87d
OweT1 <owentan@hotmail.sg>
2026-01-27 09:21:23 +0800

    model: added HuggingFace llm client for usage - OpenRouter's limit of 50 api calls a day is too little

--- src/helper/llm/constants.py
+++ src/helper/llm/constants.py
@@ -6,3 +6,7 @@ class OpenRouterFreeModels(Enum):
     DEEPSEEK = "deepseek/deepseek-r1-0528:free"
     OPENAI = "openai/gpt-oss-20b:free"
     GEMINI = "google/gemini-2.0-flash-exp:free"
+
+
+class HuggingFaceFreeModels(Enum):
+    DEEPSEEK = "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B:nscale"
--- src/helper/llm/llm_client.py
+++ src/helper/llm/llm_client.py
@@ -41,3 +41,38 @@ class OpenRouterLLMClient:
         # Extract the assistant message with reasoning_details
         response = response.choices[0].message
         return response.content
+
+
+class HuggingFaceLLMClient:
+    def __init__(
+        self,
+        api_key: str = settings.hf_api_key,
+        base_url: str = settings.hf_base_url,
+    ):
+        self.api_key = api_key
+        self.base_url = base_url
+        self.client = AsyncOpenAI(base_url=base_url, api_key=api_key)
+
+    @llm_retry_decorator
+    async def get_chat_completion(
+        self, prompt: str, model: str, reasoning_enabled: bool = True, **kwargs
+    ):
+        logger.info("Calling LLM with prompt {}", prompt)
+
+        # Create the chat.completions.create kwargs
+        completions_kwargs = {
+            "model": model,
+            "messages": [{"role": "user", "content": prompt}],
+        }
+        completions_kwargs.update(kwargs)
+
+        # Add reasoning if enabled
+        if reasoning_enabled:
+            completions_kwargs["extra_body"] = {"reasoning": {"enabled": True}}
+
+        response = await self.client.chat.completions.create(**completions_kwargs)
+        logger.info("Response generated: {}", response)
+
+        # Extract the assistant message with reasoning_details
+        response = response.choices[0].message
+        return response.content
