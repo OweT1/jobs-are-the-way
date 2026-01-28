829db8c7af9b9be04bdd436d804f9298eb89cdb2
OweT1 <owentan@hotmail.sg>
2026-01-29 01:29:05 +0800

    refactor: re-added inner helper functions for most api calls for higher resilience

--- src/helper/llm/client/base.py
+++ src/helper/llm/client/base.py
@@ -16,34 +16,40 @@ class LLMClient:
         self.base_url = base_url
         self.client = AsyncOpenAI(base_url=base_url, api_key=api_key)
 
-    @llm_retry_decorator
     async def get_chat_completion(
         self, prompt: str, model: str, reasoning_enabled: bool = True, **kwargs
     ):
         logger.info("Calling LLM with prompt {}", prompt)
 
-        # Create the chat.completions.create kwargs
-        completions_kwargs = {
-            "model": model,
-            "messages": [{"role": "user", "content": prompt}],
-        }
-        completions_kwargs.update(kwargs)
+        @llm_retry_decorator
+        async def _get_chat_completion():
+            # Create the chat.completions.create kwargs
+            completions_kwargs = {
+                "model": model,
+                "messages": [{"role": "user", "content": prompt}],
+            }
+            completions_kwargs.update(kwargs)
 
-        # Add reasoning if enabled
-        if reasoning_enabled:
-            completions_kwargs["extra_body"] = {"reasoning": {"enabled": True}}
+            # Add reasoning if enabled
+            if reasoning_enabled:
+                completions_kwargs["extra_body"] = {"reasoning": {"enabled": True}}
 
-        response = await self.client.chat.completions.create(**completions_kwargs)
-        logger.info("Response generated: {}", response)
+            response = await self.client.chat.completions.create(**completions_kwargs)
+            logger.info("Response generated: {}", response)
 
-        # Extract the message, checking for any network errors on the API
-        choice = response.choices[0]
-        if hasattr(choice, "error"):
-            logger.info("LLM Error detected")
-            error = choice.error
-            error_msg = error["message"]
-            error_code = error["code"]
-            raise Exception(f"Error code: {error_code}, Error Message: {error_msg}")
+            # Extract the message, checking for any network errors on the API
+            choice = response.choices[0]
+            if hasattr(choice, "error"):
+                logger.info("LLM Error detected")
+                error = choice.error
+                error_msg = error["message"]
+                error_code = error["code"]
+                raise Exception(f"Error code: {error_code}, Error Message: {error_msg}")
 
-        response = choice.message
-        return response.content
+            response_content = choice.message.content
+            if response_content == "":
+                logger.info("LLM Empty Output detected")
+                raise ValueError("LLM Response content should not be empty.")
+            return response_content
+
+        return await _get_chat_completion()
