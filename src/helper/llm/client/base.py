bef418a37a59f7a4599644fec373873bd54c5f54
OweT1 <owentan@hotmail.sg>
2026-03-29 18:14:17 +0800

    refactor: added structured output for LLM

--- src/helper/llm/client/base.py
+++ src/helper/llm/client/base.py
@@ -1,10 +1,13 @@
+# Standard Library Packages
+import json
+
 # Third Party Packages
 from loguru import logger
 from openai import AsyncOpenAI
+from pydantic import BaseModel
 
 # Local Project
 from src.constants import JOB_CATEGORIES
-from src.helper.retry import llm_retry_decorator
 
 
 class LLMClient:
@@ -18,39 +21,63 @@ class LLMClient:
         self.client = AsyncOpenAI(base_url=base_url, api_key=api_key)
 
     async def get_chat_completion(
-        self, prompt: str, model: str, reasoning_enabled: bool = True, **kwargs
-    ):
+        self,
+        prompt: str,
+        model: str,
+        reasoning_enabled: bool = True,
+        response_format: BaseModel | dict | None = None,
+        **kwargs,
+    ) -> BaseModel | str:
         logger.info("Calling LLM with prompt {}", prompt)
 
-        @llm_retry_decorator
-        async def _get_chat_completion():
-            # Create the chat.completions.create kwargs
-            completions_kwargs = {
-                "model": model,
-                "messages": [{"role": "user", "content": prompt}],
-            }
-            completions_kwargs.update(kwargs)
+        # @llm_retry_decorator
+        async def _get_chat_completion(response_format=response_format):
+            try:
+                # Create the chat.completions.create kwargs
+                if issubclass(response_format, BaseModel):
+                    response_schema = response_format.model_json_schema()
+                    response_schema["additionalProperties"] = False
+                    response_format = {
+                        "type": "json_schema",
+                        "json_schema": {
+                            "name": response_schema["title"],
+                            "strict": True,
+                            "schema": response_schema,
+                        },
+                    }
 
-            # Add reasoning if enabled
-            if reasoning_enabled:
-                completions_kwargs["extra_body"] = {"reasoning": {"enabled": True}}
+                completions_kwargs = {
+                    "model": model,
+                    "messages": [{"role": "user", "content": prompt}],
+                    "response_format": response_format,
+                }
+                completions_kwargs.update(kwargs)
 
-            response = await self.client.chat.completions.create(**completions_kwargs)
-            logger.info("Response generated: {}", response)
+                # Add reasoning if enabled
+                if reasoning_enabled:
+                    completions_kwargs["extra_body"] = {"reasoning": {"enabled": True}}
 
-            # Extract the message, checking for any network errors on the API
-            choice = response.choices[0]
-            if hasattr(choice, "error"):
-                logger.info("LLM Error detected.")
-                error = choice.error
-                error_msg = error["message"]
-                error_code = error["code"]
-                raise Exception(f"Error code: {error_code}, Error Message: {error_msg}")
+                response = await self.client.chat.completions.create(**completions_kwargs)
+                logger.info("Response Generated: {}", response)
 
-            response_content = choice.message.content.strip()
-            if response_content not in JOB_CATEGORIES:
-                logger.info("LLM Wrong Output detected.")
-                raise ValueError(f"LLM Response content should be in {JOB_CATEGORIES}.")
-            return response_content
+                # Extract the message, checking for any network errors on the API
+                choice = response.choices[0]
+                if hasattr(choice, "error"):
+                    logger.info("LLM Error detected.")
+                    error = choice.error
+                    error_msg = error["message"]
+                    error_code = error["code"]
+                    raise Exception(f"Error code: {error_code}, Error Message: {error_msg}")
+
+                response_content = choice.message.content.strip()
+                response_content_parsed = json.loads(response_content)
+                job_category = response_content_parsed["job_category"]
+                if job_category not in JOB_CATEGORIES:
+                    logger.info("LLM Wrong Output detected.")
+                    raise ValueError(f"LLM Response content should be in {JOB_CATEGORIES}.")
+                return job_category
+            except Exception as e:
+                logger.error("Exception occurred: {}", e)
+                raise Exception(e)
 
         return await _get_chat_completion()
