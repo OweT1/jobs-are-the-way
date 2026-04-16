82ca362a740a2e3f9f6f2a46bd3c7f7d5e97dc97
OweT1 <owentan@hotmail.sg>
2026-04-16 21:46:18 +0800

    fix: refactor search_jobs to handle unknown errors

--- src/helper/job_search.py
+++ src/helper/job_search.py
@@ -9,21 +9,29 @@ from src.helper.retry import job_search_retry_decorator
 
 
 # --- Functions --- #
-@job_search_retry_decorator
 def search_jobs(
     search_term: str,
     hours_old: int,
     location: str = settings.default_location,
 ) -> pd.DataFrame:
-    jobs = scrape_jobs(
-        site_name=["indeed", "linkedin", "zip_recruiter", "google"],
-        search_term=search_term,
-        google_search_term=f"{search_term} jobs in {location} since {hours_old} hours ago",
-        location=location,
-        country_indeed=location,
-        hours_old=hours_old,
-        linkedin_fetch_description=True,  # gets more info such as description, direct job url (slower)
-        # proxies=["208.195.175.46:65095", "208.195.175.45:65095"],
-    )
-    logger.info("Found {} jobs for search term {}", len(jobs), search_term)
-    return jobs
+    @job_search_retry_decorator
+    def _search_jobs():
+        jobs = scrape_jobs(
+            site_name=["indeed", "linkedin", "zip_recruiter", "google"],
+            search_term=search_term,
+            google_search_term=f"{search_term} jobs in {location} since {hours_old} hours ago",
+            location=location,
+            country_indeed=location,
+            hours_old=hours_old,
+            linkedin_fetch_description=True,  # gets more info such as description, direct job url (slower)
+            # proxies=["208.195.175.46:65095", "208.195.175.45:65095"],
+        )
+        logger.info("Found {} jobs for search term {}", len(jobs), search_term)
+        return jobs
+
+    try:
+        jobs = _search_jobs()
+        return jobs
+    except Exception as e:
+        logger.info("An error occurred: {}. Skipping for {}...", e, search_term)
+        return pd.DataFrame()
